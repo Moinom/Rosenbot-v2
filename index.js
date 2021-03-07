8e2bd4ec98b51f5a6e2adf298d2d6e44cddd5257
Moinom <st.lisa93@yahoo.de>
2021-03-08 00:38:26 +0100

    Added new streamer. Implemented async functions. Bot now knows game names.

--- index.js
+++ index.js
@@ -7,61 +7,26 @@ client.on('ready', () => {
 
 	const stream_channel = client.channels.cache.get(config.streamChannelID);
 	var all_streamers = config.streamerNames;
+	
+	// loop through all the streamers in config and check their profiles
 	function streamerLoop(){
-		Object.keys(all_streamers).forEach(key => pingTwitch(key, all_streamers[key]));
-	}
-
-	function pingTwitch(streamer, isOnline){
-		
-		// getting OAuth client credentials
-		fetch(config.oAuthLink, {method: 'POST'})
-			
-			.then(res => res.json())
-			.then(res => {
-				// getting oAuth token
-			    var token = res.access_token;
-
-			    // authorisation info
-			    var auth = {  
-					method: 'GET',
-					headers: { 
-						'client-id': config.oAuthClientID, 
-						'Authorization': 'Bearer ' + token
-					} 
-				}
-
-				// fetch streamer channel
-				fetch('https://api.twitch.tv/helix/search/channels?query='+ streamer, auth)
-					.then(res => res.json())
-					.then(pull => {
-						var data;
-						for (let i in pull.data){
-							if (pull.data[i].broadcaster_login == streamer.toLowerCase()){
-								data = pull.data[i];
-							}
-						}
-						
-						if (data != null){
-							getStreamData(data, isOnline, streamer)
-						}
-				})
-			})
+		Object.keys(all_streamers).forEach(key => findStreamer(key, all_streamers[key]));
 	}
 	
-	function getStreamData(_streamData, _isOnline, _streamer){
+	// collect stream info, starts announcer if stream is live
+	async function checkStream(_streamData, _isOnline, _streamer){
 		
 		var status = _streamData.is_live; 
 		var logo = _streamData.thumbnail_url;
 		var title = _streamData.title;
-		//var gameID = _streamData.game_id;
-		//var game = getGame(gameID);
-		console.log(_streamData)
+		var gameID = _streamData.game_id;
+		var game = await getGame(gameID).catch(e => { console.log(e) });
 
 		// only run when online and not already announced
 		if ((status != _isOnline) && (status)){
 			
 			all_streamers[_streamer] = true;
-			announcer(_streamer, title, logo);
+			announcer(_streamer, title, logo, game);
 
 		// mark as offline after stream ended
 		} else if (!status && _isOnline){
@@ -69,39 +34,71 @@ client.on('ready', () => {
 		}		
 	}
 	
-	/*
-	function getGame(_gameID){
-		var requestString = "https://api.twitch.tv/helix/games?id=" + _gameID;
-		var game;
+	// find streamer profile from API
+	async function findStreamer(_streamer, _isOnline) {
+
+		const url = 'https://api.twitch.tv/helix/search/channels?query=' + _streamer;
+		const streamObj = await apiCall(url)
 		
-		fetch(config.oAuthLink, {method: 'POST'})
-			
-			.then(res => res.json())
-			.then(res => {
-				// getting oAuth token
-			    var token = res.access_token;
-
-			    // authorisation info
-			    var auth = {  
-					method: 'GET',
-					headers: { 
-						'client-id': config.oAuthClientID, 
-						'Authorization': 'Bearer ' + token
-					} 
-				}
-
-				// fetch streamer channel
-				fetch(requestString, auth)
-					.then(res => res.json())
-					.then(pull => {
-						game = pull.data[0].name;
-						//console.log(game)
-						return game
-				})
-			})	
-	} */
+		var data;
+		for (let i in streamObj.data){
+			if (streamObj.data[i].broadcaster_login == _streamer.toLowerCase()){
+				data = streamObj.data[i];
+				console.log(data)
+			}
+		}
+		if (data != null){
+			checkStream(data, _isOnline, _streamer)
+		}
+	}
 	
-	function announcer(_streamer, _title, _logo){
+	// find out game name
+	async function getGame(_gameID){
+		
+		const url = "https://api.twitch.tv/helix/games?id=" + _gameID;
+		const gameObj = await apiCall(url);
+		
+		var game = "";
+		
+		if (gameObj != null && gameObj != 0){
+			game = gameObj.data[0].name;
+		}
+		return game
+	}
+	
+	// generate auth token for API call
+	async function getAuthToken(){
+
+		const authResponse = await fetch(config.oAuthLink, {method: 'POST'});
+		const authResJson = await authResponse.json();
+		var token = await authResJson.access_token;
+
+		return token
+	}
+
+	// get json object from API
+	async function apiCall(_url){
+
+		const token = await getAuthToken();
+		var auth = {  
+			method: 'GET',
+			headers: { 
+				'client-id': config.oAuthClientID, 
+				'Authorization': 'Bearer ' + token
+				} 
+			}
+		const response = await fetch(_url, auth);
+		const obj = await response.json();
+
+		return obj	
+	}
+	
+	// announce in discord
+	function announcer(_streamer, _title, _logo, _game){
+		
+		if (_game != null){
+			_game = " " + _game;
+		}
 
 		stream_channel.send({
 			content: "Attention, attention! Stream alert! :alarm_clock:",
@@ -111,7 +108,7 @@ client.on('ready', () => {
 			      name: _streamer,
 			      icon_url: _logo
 			    },
-			    title: _streamer + " is now streaming! Go and watch the stream!",
+			    title: _streamer + " is now streaming"+ _game +"! Go and watch the stream!",
 			    thumbnail: {
 						url: _logo
 					},
@@ -120,7 +117,6 @@ client.on('ready', () => {
 		    }
 		});		
 	}
-
 	setInterval(streamerLoop, 30000);
 });
 
