cb3bbab1a5c14da911caf234a1563a6617b98645
Lisa <st.lisa93@yahoo.de>
2023-09-10 19:58:01 +0200

    automatic subscription when streamer is added

--- src/commands/announcer/add-streamer.ts
+++ src/commands/announcer/add-streamer.ts
@@ -1,6 +1,8 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { createStreamer } from '../../database';
+import { createSubscription, getSubscriptions, requestStreamerId } from '../../twitch';
 
+const TWITCH_CALLBACK_URL = process.env.TWITCH_CALLBACK_URL || '';
 export const data = new SlashCommandBuilder()
   .setName('add-streamer')
   .setDescription('Add a streamer to the stream announcer.')
@@ -11,8 +13,38 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: CommandInteraction) {
   if (!interaction.isChatInputCommand()) return;
   const name: string | null = interaction.options.getString('twitch-name');
+
   if (name) {
-    const createResponse = await createStreamer(name);
+    // Get ID associated with Twitch name
+    const streamerId = await requestStreamerId(name);
+    if (!streamerId) {
+      await interaction.reply(`${name} was not found on Twitch.`);
+      return;
+    }
+    // Check if subsription for this user already exists
+    const subscriptions = await getSubscriptions(`user_id=${streamerId}`);
+    let subscriptionId = '';
+    const userSub = subscriptions.data;
+
+    if (
+      userSub.length > 0 &&
+      userSub[0].type === 'stream.online' &&
+      userSub[0].status === 'enabled' &&
+      userSub[0].transport.callback === TWITCH_CALLBACK_URL
+    ) {
+      subscriptionId = subscriptions.data[0].id;
+    } else {
+      // Subscribe to Twitch channel activity if no subscription yet
+      subscriptionId = await createSubscription(streamerId);
+    }
+
+    if (!subscriptionId) {
+      await interaction.reply(`Subscribing to Twitch channel ${name} failed.`);
+      return;
+    }
+
+    // Add streamer to database
+    const createResponse = await createStreamer(name, streamerId, subscriptionId);
     switch (createResponse) {
       case 'success': {
         await interaction.reply(`${name} has been added to the announcer.`);
@@ -20,10 +52,6 @@ export async function execute(interaction: CommandInteraction) {
       }
       case 'duplicate': {
         await interaction.reply(`${name} is already part of the announcer.`);
-        return;
-      }
-      case 'notFound': {
-        await interaction.reply(`${name} was not found on Twitch.`);
         return;
       }
     }
