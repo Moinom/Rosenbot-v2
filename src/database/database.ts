578ce544c91cbeb27ff12f285c9d687402b5aa26
Lisa <st.lisa93@yahoo.de>
2023-11-12 00:55:46 +0100

    update database variables with azure names

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db_connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.AZURE_MYSQL_HOST,
+  user: process.env.AZURE_MYSQL_USER,
+  password: process.env.AZURE_MYSQL_PASSWORD,
+  database: process.env.AZURE_MYSQL_DATABASE,
 });
 
 db_connection.connect();
